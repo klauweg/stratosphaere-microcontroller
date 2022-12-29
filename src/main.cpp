412388b4041587ccab34ce205fdccbb1d2e9dba7
PlaceblockHD <felix.weglehner@gmx.de>
2022-12-29 09:12:29 +0100

    Implemented Sensors

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,18 @@
 #include "sensor/mpu/MPUSensor.h"
 #include "sensor/ms5/MS5Sensor.h"
 #include <iostream>
-using namespace std;
+#include <Arduino.h>
 
 //..\arduino-cli.exe compile strato.ino --fqbn esp32:esp32:t-beam
 HIHSensor *hihSensor = new HIHSensor();
-GPSSensor *gpsSensor = new GPSSensor();
+//GPSSensor *gpsSensor = new GPSSensor();
 LM75Sensor *lm75Sensor = new LM75Sensor();
 MPUSensor *mpuSensor = new MPUSensor();
 MS5Sensor *ms5Sensor = new MS5Sensor();
 
 void setup() {
   hihSensor->configure();
-  gpsSensor->configure();
+  //gpsSensor->configure();
   lm75Sensor->configure();
   mpuSensor->configure();
   ms5Sensor->configure();
@@ -23,8 +23,9 @@ void setup() {
 
 void loop() {
   HIHData hihData = hihSensor->getData();
-  GPSData gpsData = gpsSensor->getData();
+  //GPSData gpsData = gpsSensor->getData();
   LM75Data lm75Data = lm75Sensor->getData();
   MPUData mpuDataData = mpuSensor->getData();
   MS5Data ms5Data = ms5Sensor->getData();
+  delay(1000);
 }
--- src/sensor/ms5/MS5Sensor.h
+++ src/sensor/ms5/MS5Sensor.h
@@ -3,8 +3,9 @@
 class MS5Data : public SensorData {
     private:
         uint16_t pressure;
+        uint16_t temperature;
     public:
-        MS5Data(uint16_t);
+        MS5Data(uint16_t, uint16_t);
         uint8_t convertLORA();
 };
 
