a2717803b89ad46252e2ff2c9825765e152b0f06
PlaceblockHD <felix.weglehner@gmx.de>
2022-12-30 00:26:36 +0100

    serial problem

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,21 @@
-#include "sensor/hih/HIHSensor.h"
-#include "sensor/gps/GPSSensor.h"
-#include "sensor/lm75/LM75Sensor.h"
-#include "sensor/mpu/MPUSensor.h"
-#include "sensor/ms5/MS5Sensor.h"
-#include <Arduino.h>
-#include <Wire.h>
-#include "SD_MMC.h"
+//#include "utilities.h"
+#include <serial.h>
 
-long lastMillis = 0;
-
-//..\arduino-cli.exe compile strato.ino --fqbn esp32:esp32:t-beam
-HIHSensor *hihSensor = new HIHSensor();
-GPSSensor *gpsSensor = new GPSSensor();
-LM75Sensor *lm75Sensor = new LM75Sensor();
-MPUSensor *mpuSensor = new MPUSensor();
-MS5Sensor *ms5Sensor = new MS5Sensor();
-
-void setup() {
-  Serial.begin(115200);
-  Wire.begin();
-  Wire.setClock(100000);
-
-  hihSensor->configure();
-  gpsSensor->configure();
-  lm75Sensor->configure();
-  mpuSensor->configure();
-  ms5Sensor->configure();
+void setup()
+{
+   //initBoard();
+   Serial.begin(115200);
+   Serial1.begin(9600);
 }
 
-void loop() {
-  Serial.println("Last Loop Duration: ");
-  Serial.println(millis()-lastMillis);
-  lastMillis = millis();
-  //HIHData hihData = hihSensor->getData();
-  GPSData gpsData = gpsSensor->getData();
-  //LM75Data lm75Data = lm75Sensor->getData();
-  MPUData mpuData = mpuSensor->getData();
-  MS5Data ms5Data = ms5Sensor->getData();
-  /*Serial.println("-----------------");
-  if (gpsData.time.hour() < 10) Serial.print(F("0"));
-  Serial.print(gpsData.time.hour());
-  Serial.print(":");
-  if (gpsData.time.minute() < 10) Serial.print(F("0"));
-  Serial.print(gpsData.time.minute());
-  Serial.print(":");
-  if (gpsData.time.second() < 10) Serial.print(F("0"));
-  Serial.print(gpsData.time.second());
-  Serial.print(".");
-  if (gpsData.time.centisecond() < 10) Serial.print(F("0"));
-  Serial.println(gpsData.time.centisecond());
-  Serial.println(gpsData.location.lat());
-  Serial.println(gpsData.location.lng());
-  Serial.println(gpsData.satellites.value());
-  Serial.println("-----------------");*/
-  //Serial.println(mpuData.temperature);
-  //Serial.println(ms5Data.pressure);
-  //Serial.println(ms5Data.temperature);
-  //Serial.println(ms5Sensor->correct(ms5Data).pressure);
-  //Serial.println(ms5Sensor->correct(ms5Data).temperature);
-  delay(10000);
+void loop()
+{
+  while (Serial.available())
+  {
+    Serial1.write(Serial.read());
+  }
+  while (Serial1.available())
+  {
+    Serial.write(Serial1.read());
+  }
 }
