4836adcba31bbdb762b4b4495c1a31e7fbd4b6c9
PlaceblockHD <felix.weglehner@gmx.de>
2023-05-16 20:58:07 +0200

    Implemented new MS5 Data Correction

--- src/sensor/ms5/MS5Sensor.cpp
+++ src/sensor/ms5/MS5Sensor.cpp
@@ -55,52 +55,96 @@ int32_t MS5Sensor::getMeasurement(measurement _measurement) {
 }
 
 void MS5Sensor::correct() {
-  	int32_t temperature_raw = this->data.getTemperature();
-	int32_t pressure_raw = this->data.getPressure();
-	
-  	//Create Variables for calculations
-	int32_t temp_calc;
-	int32_t pressure_calc;
-	
-	int32_t dT;
-		
-	//Now that we have a raw temperature, let's compute our actual.
-	dT = temperature_raw - ((int32_t)coefficient[5] << 8);
-	temp_calc = (((int32_t)dT * coefficient[6]) >> 23) + 2000;
-	
-	//Now we have our first order Temperature, let's calculate the second order.
-	int64_t T3, OFF2, SENS2, OFF, SENS; //working variables
 
-	if (temp_calc < 2000) {
-        // If temp_calc is below 20.0C
-        T3 = 3 * (((int64_t)dT * dT) >> 33);
-		OFF2 = 3 * ((temp_calc - 2000) * (temp_calc - 2000)) / 2;
-		SENS2 = 5 * ((temp_calc - 2000) * (temp_calc - 2000)) / 8;
-		
-		if(temp_calc < -1500) {
-            // If temp_calc is below -15.0C
-			OFF2 = OFF2 + 7 * ((temp_calc + 1500) * (temp_calc + 1500));
-			SENS2 = SENS2 + 4 * ((temp_calc + 1500) * (temp_calc + 1500));
-		}
-    } else {
-        // If temp_calc is above 20.0C
-        T3 = 7 * ((uint64_t)dT * dT) / pow(2, 37);
-		OFF2 = ((temp_calc - 2000) * (temp_calc - 2000)) / 16;
-		SENS2 = 0;
-	}
-	
-	// Now bring it all together to apply offsets
-	OFF = ((int64_t)coefficient[2] << 16) + (((coefficient[4] * (int64_t)dT)) >> 7);
-	SENS = ((int64_t)coefficient[1] << 15) + (((coefficient[3] * (int64_t)dT)) >> 8);
-	
-	temp_calc = temp_calc - T3;
-	OFF = OFF - OFF2;
-	SENS = SENS - SENS2;
+    // Create array to hold the 8 sensor calibration coefficients
+    //static unsigned int      sensorCoeffs[8]; // unsigned 16-bit integer (0-65535)
+    // D1 and D2 need to be unsigned 32-bit integers (long 0-4294967295)
+    uint32_t     D1 = this->data.getPressure();    // Store uncompensated pressure value
+    uint32_t     D2 = this->data.getTemperature();    // Store uncompensated temperature value
+    // These three variables are used for the conversion steps
+    // They should be signed 32-bit integer initially
+    // i.e. signed long from -2147483648 to 2147483647
+    int32_t	dT;
+    int32_t 	TEMP;
+    // These values need to be signed 64-bit integers
+    // (long = int64_t)
+    int64_t	Offset;
+    int64_t	Sensitivity;
+    int64_t	T22;
+    int64_t	OFF2;
+    int64_t	Sens2;
 
-	// Now let's calculate the pressure
-	pressure_calc = ((SENS * pressure_raw) / 2097152 - OFF) / 32768;
+    // Calculate 1st order temperature, dT is a long signed integer
+    // D2 is originally cast as an uint32_t, but can fit in a int32_t, so we'll
+    // cast both parts of the equation below as signed values so that we can
+    // get a negative answer if needed
+    dT = (int32_t)D2 - ( (int32_t)this->coefficient[5] * 256 );
+    // Use integer division to calculate TEMP. It is necessary to cast
+    // one of the operands as a signed 64-bit integer (int64_t) so there's no
+    // rollover issues in the numerator.
+    TEMP = 2000 + ((int64_t)dT * this->coefficient[6]) / 8388608LL;
+    // Recast TEMP as a signed 32-bit integer
+    TEMP = (int32_t)TEMP;
 
-  	this->correctedData = MS5CorrectedData{pressure_calc/10.0f, temp_calc/100.0f};
+
+    // All operations from here down are done as integer math until we make
+    // the final calculation of pressure in mbar.
+
+
+    // Do 2nd order temperature compensation (see pg 9 of MS5803 data sheet)
+    // I have tried to insert the fixed values wherever possible
+    // (i.e. 2^31 is hard coded as 2147483648).
+    if (TEMP < 2000) {
+        // For 1 bar model
+        T22 = ((int64_t)dT * dT) / 2147483648ULL ; // 2^31 = 2147483648
+        T22 = (int32_t)T22; // recast as signed 32bit integer
+        OFF2 = 3 * ((TEMP-2000) * (TEMP-2000));
+        Sens2 = 7 * ((TEMP-2000)*(TEMP-2000)) / 8 ;
+    } else { // if TEMP is > 2000 (20.0C)
+        // For 1 bar model
+        T22 = 0;
+        OFF2 = 0;
+        Sens2 = 0;
+        if (TEMP > 4500) {
+            // Extra adjustment for high temps, only needed for 1 bar model
+            Sens2 = Sens2 - ((TEMP-4500)*(TEMP-4500)) / 8;
+        }
+    }
+
+    // Additional compensation for very low temperatures (< -15C)
+    if (TEMP < -1500) {
+        // For 1 bar model
+        // Leave OFF2 alone in this case
+        Sens2 = Sens2 + 2 * ((TEMP+1500)*(TEMP+1500));
+    }
+
+    // Calculate initial Offset and Sensitivity
+    // Notice lots of casts to int64_t to ensure that the
+    // multiplication operations don't overflow the original 16 bit and 32-bit
+    // integers
+
+    // For 1 bar sensor
+    Offset = (int64_t)this->coefficient[2] * 65536 + (this->coefficient[4] * (int64_t)dT) / 128;
+    Sensitivity = (int64_t)this->coefficient[1] * 32768 + (this->coefficient[3] * (int64_t)dT) / 256;
+
+    // Adjust TEMP, Offset, Sensitivity values based on the 2nd order
+    // temperature correction above.
+    TEMP = TEMP - T22; // both should be int32_t
+    Offset = Offset - OFF2; // both should be int64_t
+    Sensitivity = Sensitivity - Sens2; // both should be int64_t
+
+    // Final compensated pressure calculation. We first calculate the pressure
+    // as a signed 32-bit integer (mbarInt), then convert that value to a
+    // float (mbar).
+
+    // For 1 bar sensor
+    int32_t mbarInt = ((D1 * Sensitivity) / 2097152 - Offset) / 32768;
+    float mbar = (float)mbarInt / 100;
+
+    // Calculate the human-readable temperature in Celsius
+    float tempC  = (float)TEMP / 100;
+
+  	this->correctedData = MS5CorrectedData{mbar, tempC};
 }
 
 void MS5Data::print(MS5CorrectedData corrected) const {
